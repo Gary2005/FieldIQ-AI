825d1bb70538e75e1489062beca730cbc4181644
Gary2005 <2803175673@qq.com>
2025-05-17 17:52:27 +0800

    new camera

--- get_annotation_p.py
+++ get_annotation_p.py
@@ -5,8 +5,8 @@ from PIL import Image
 import matplotlib.pyplot as plt  
 import torchvision.transforms as T  
 from pathlib import Path  
-  
-# Import the necessary modules from the repository  
+
+
 from plugins.calibration.nbjw_calib.model.cls_hrnet import get_cls_net  
 from plugins.calibration.nbjw_calib.model.cls_hrnet_l import get_cls_net as get_cls_net_l  
 from plugins.calibration.nbjw_calib.utils.utils_heatmap import (  
@@ -16,47 +16,6 @@ from plugins.calibration.nbjw_calib.utils.utils_heatmap import (
     coords_to_dict  
 )  
 from tracklab.utils.download import download_file  
-  
-# Function to convert keypoints to lines (from the repository)  
-def kp_to_line(keypoints):  
-    line_keypoints_match = {"Big rect. left bottom": [24, 68, 25],  
-                           "Big rect. left main": [5, 64, 31, 46, 34, 66, 25],  
-                           "Big rect. left top": [4, 62, 5],  
-                           "Big rect. right bottom": [26, 69, 27],  
-                           "Big rect. right main": [6, 65, 33, 56, 36, 67, 26],  
-                           "Big rect. right top": [6, 63, 7],  
-                           "Circle central": [32, 48, 38, 50, 42, 53, 35, 54, 43, 52, 39, 49],  
-                           "Circle left": [31,37, 47, 41, 34],  
-                           "Circle right": [33, 40, 55, 44, 36],  
-                           "Goal left crossbar": [16, 12],  
-                           "Goal left post left": [16, 17],  
-                           "Goal left post right": [12, 13],  
-                           "Goal right crossbar": [15, 19],  
-                           "Goal right post left": [15, 14],  
-                           "Goal right post right": [19, 18],  
-                           "Middle line": [2, 32, 51, 35, 29],  
-                           "Side line bottom": [28, 70, 71, 29, 72, 73, 30],  
-                           "Side line left": [1, 4, 8, 13,17, 20, 24, 28],  
-                           "Side line right": [3, 7, 11, 14, 18, 23, 27, 30],  
-                           "Side line top": [1, 58, 59, 2, 60, 61, 3],  
-                           "Small rect. left bottom": [20, 21],  
-                           "Small rect. left main": [9, 21],  
-                           "Small rect. left top": [8, 9],  
-                           "Small rect. right bottom": [22, 23],  
-                           "Small rect. right main": [10, 22],  
-                           "Small rect. right top": [10, 11]}  
-  
-    lines = {}  
-    for line_name, kp_indices in line_keypoints_match.items():  
-        line = []  
-        for idx in kp_indices:  
-            if idx in keypoints.keys():  
-                line.append({'x': keypoints[idx]['x'], 'y': keypoints[idx]['y']})  
-  
-        if line:  
-            lines[line_name] = line  
-  
-    return lines  
 
 def kp_to_realworld(keypoints):  
     # Define the world coordinates for the main keypoints (57 points)  
@@ -110,68 +69,15 @@ def kp_to_realworld(keypoints):
       
     return real_world_coords
 
-  
-# Function to visualize the detected lines  
-def visualize_lines(image, lines):  
-    plt.figure(figsize=(12, 8))  
-    plt.imshow(image)  
-      
-    colors = plt.cm.tab20(np.linspace(0, 1, len(lines)))  
-      
-    for i, (line_name, points) in enumerate(lines.items()):  
-        if len(points) >= 2:  
-            x_coords = [p['x'] * image.width for p in points]  
-            y_coords = [p['y'] * image.height for p in points]  
-            plt.plot(x_coords, y_coords, '-', color=colors[i], linewidth=2, label=line_name)  
-      
-    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')  
-    plt.tight_layout()  
-    plt.savefig('detected_lines.png')  
-    plt.show()  
-
-
-def load_models():
-    import yaml
-    with open('plugins/calibration/nbjw_calib/config/hrnetv2_w48.yaml', 'r') as f:  
-        cfg = yaml.safe_load(f)  
-      
-    with open('plugins/calibration/nbjw_calib/config/hrnetv2_w48_l.yaml', 'r') as f:  
-        cfg_l = yaml.safe_load(f)
-      
-    # Create model directory if it doesn't exist  
-    model_dir = Path("./models")  
-    model_dir.mkdir(exist_ok=True)  
-      
-    # Paths for the checkpoint files  
-    checkpoint_kp = model_dir / "SV_kp"  
-    checkpoint_l = model_dir / "SV_lines"  
-      
-    # Download the models if they don't exist  
-    if not os.path.isfile(checkpoint_kp):  
-        download_file("https://zenodo.org/records/12626395/files/SV_kp?download=1", checkpoint_kp)  
-      
-    if not os.path.isfile(checkpoint_l):  
-        download_file("https://zenodo.org/records/12626395/files/SV_lines?download=1", checkpoint_l)  
-      
-    # Load the models  
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  
-    # print(f"Using device: {device}")
-      
-    model = get_cls_net(cfg)  
-    model.load_state_dict(torch.load(checkpoint_kp, map_location=device))  
-    model.to(device)  
-    model.eval()  
-      
-    model_l = get_cls_net_l(cfg_l)  
-    model_l.load_state_dict(torch.load(checkpoint_l, map_location=device))  
-    model_l.to(device)  
-    model_l.eval()  
-
-    return model, model_l
-
 def run_demo(images, model, model_l):  
 
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # print(f"Available GPUs: {torch.cuda.device_count()}")  # 打印有几张卡
+    # print(f"Model on GPUs: {model.device_ids}")
+
+    model = torch.nn.DataParallel(model).to(device)
+    model_l = torch.nn.DataParallel(model_l).to(device)
     
     # Preprocess all images and stack them into a batch
     tfms_resize = T.Compose([  
@@ -227,15 +133,3 @@ def run_demo(images, model, model_l):
         annotations.append(lines)
 
     return annotations
-
-
-if __name__ == "__main__":
-    paths = [
-        "calibration-2023/train/00000.jpg",
-        "calibration-2023/train/00001.jpg",
-        "calibration-2023/train/00002.jpg",
-        "calibration-2023/train/00003.jpg",
-    ]
-    images = [Image.open(path).convert("RGB") for path in paths]
-    annotations = run_demo(images)
-    print(annotations)
---
+++ segment_time_camera.py
@@ -0,0 +1,137 @@
+import cv2
+
+from get_annotation import load_models, run_demo
+from process_dataset import process_anno
+from tqdm import tqdm
+
+model, model_l = load_models()
+
+
+def process(frames, fps):
+    images = [image for image, _ in frames]
+    annotations = run_demo(images, model, model_l)
+    results = []
+    for i, (image, frame_idx) in enumerate(frames):
+        annotation = annotations[i]
+        if annotation is None:
+            continue
+        matrix, error = process_anno(annotation)
+        t = frame_idx / fps
+        result = {
+            "time": t,
+            "htime": f"{int(t // 60)}:{int(t % 60):02d}",
+            "frame": frame_idx,
+            "matrix": (matrix.tolist() if matrix is not None else None),
+            "error": error,
+        }
+        results.append(result)
+    return results
+
+def process_time_segment(video_path, start_time, end_time):
+
+    batch_size = 10
+
+
+    """
+    Process the video segment between start_time and end_time.
+    """
+    # Load the video
+    video = cv2.VideoCapture(video_path)
+    
+    # Get the frames per second (fps) of the video
+    fps = video.get(cv2.CAP_PROP_FPS)
+    
+    # Calculate the start and end frame numbers
+    start_frame = int(start_time * fps)
+    end_frame = int(end_time * fps)
+    
+    # Set the video to the start frame
+    video.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+    
+    frames = []
+    results = []
+    
+    # Read frames until we reach the end frame
+
+    bar = tqdm(total=end_frame - start_frame, desc="Processing frames", unit="frame")
+
+
+    while True:
+        ret, frame = video.read()
+        if not ret or video.get(cv2.CAP_PROP_POS_FRAMES) > end_frame:
+            break
+        current_frame_number = int(video.get(cv2.CAP_PROP_POS_FRAMES))
+        frames.append((frame, current_frame_number))
+
+        if len(frames) == batch_size:
+            batch_results = process(frames, fps)
+            results.extend(batch_results)
+            frames = []
+
+        bar.update(1)
+
+    if len(frames) > 0:
+        batch_results = process(frames, fps)
+        results.extend(batch_results)
+    video.release()
+    bar.close()
+
+    return results
+    
+if __name__ == "__main__":
+    video_path = "test_video"
+
+    first_half_path = f"{video_path}/1_720p.mkv"
+    second_half_path = f"{video_path}/2_720p.mkv"
+    label_camera_path = f"{video_path}/Labels-cameras.json"
+
+    output_path = f"{video_path}/output.json"
+
+    compute_segments = []
+
+    import json
+
+    label_camera_json = json.load(open(label_camera_path, "r"))
+
+    for index, label in enumerate(label_camera_json["annotations"]):
+        if label["replay"] == "real-time" and label["label"].startswith("Main"):
+            pre_time = None
+            if index > 0:
+                pre_time = label_camera_json["annotations"][index - 1]["gameTime"]
+            else:
+                pre_time = "1 - 00:00"
+            now_time = label_camera_json["annotations"][index]["gameTime"]
+            if pre_time[0] != now_time[0]:
+                pre_time = "2 - 00:00"
+                assert pre_time[0] == now_time[0], f"pre_time: {pre_time}, now_time: {now_time}"
+            compute_segments.append((pre_time, now_time))
+
+    total_time = 0
+    for segment in compute_segments:
+        start_time = segment[0].split(" - ")[1]
+        end_time = segment[1].split(" - ")[1]
+        start_time = int(start_time.split(":")[0]) * 60 + int(start_time.split(":")[1])
+        end_time = int(end_time.split(":")[0]) * 60 + int(end_time.split(":")[1])
+        total_time += end_time - start_time
+
+    print(f"Total time: {total_time} seconds")
+
+    bar = tqdm(total=total_time, desc="Processing video segments", unit="s")
+
+    processed_results = {}
+
+    for segment in compute_segments:
+        video_path = first_half_path if segment[0][0] == "1" else second_half_path
+        
+        start_str = segment[0].split(" - ")[1]
+        end_str   = segment[1].split(" - ")[1]
+        start_seconds = int(start_str.split(":")[0]) * 60 + int(start_str.split(":")[1])
+        end_seconds   = int(end_str.split(":")[0])   * 60 + int(end_str.split(":")[1])
+
+        processed_results[f"{segment}"] = process_time_segment(video_path, start_seconds, end_seconds)
+
+        with open(output_path, "w") as f:
+            json.dump(processed_results, f, indent=4)
+
+        bar.update(end_seconds - start_seconds)
+    bar.close()
